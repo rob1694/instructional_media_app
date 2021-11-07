9382bedb3fa3030d4f26306cd415254666c38cc8
rob1694 <robertb1694@gmail.com>
2021-11-07 10:06:42 -0500

    heroku ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,28 +10,45 @@ u1 = User.create(username: "monkey123")
 
 u2 = User.create(username: "thatGuy345")
 
-p1 = Post.create([{
+p1 = Post.create(
     user_id: 1,
-    title: "cookies", 
-    description: "bake this!", 
+    title: "Chocolate Chip Cookies", 
+    description: "Awsome Chocolate Chip Cookies to try the next time your craving!", 
     imgs: ["img1", "img2"], 
-    steps: ["step1", "step2"], 
-    items: ["item1", "item2"]}])
+    steps: ["Heat oven to 375°F. In small bowl, mix flour, baking soda and salt; set aside.",
+            "step2",
+            "In large bowl, beat softened butter and sugars with electric mixer on medium speed, or mix with spoon about 1 minute or until fluffy, scraping side of bowl occasionally.",
+            "Beat in egg and vanilla until smooth. Stir in flour mixture just until blended (dough will be stiff). Stir in chocolate chips and nuts.",
+            "Onto ungreased cookie sheets, drop dough by rounded tablespoonfuls 2 inches apart.",
+            "Bake 8 to 10 minutes or until light brown (centers will be soft). Cool 2 minutes; remove from cookie sheet to cooling rack. Cool completely, about 30 minutes. Store covered in airtight container."
+        ], 
+    items: ["2 1/4 cups Gold Medal™ all-purpose flour",
+            "1 teaspoon baking soda",
+            "1/2 teaspoon salt",
+            "1 cup butter, softened",
+            "3/4 cup granulated sugar",
+            "3/4 cup packed brown sugar",
+            "1 egg",
+            "1 teaspoon vanilla",
+            "2 cups semisweet chocolate chips",
+            "1 cup coarsely chopped nuts (optional)"
+        ]
+    )
 
 p2 = Post.create(
     user_id: 2,
-    title: "brownie", 
-    description: "Awsome!", 
+    title: "Paper Airplane", 
+    description: "Fast Glider!, A fun looking plane that flies and glides perfectly. (Difficulty: Medium", 
     imgs: ["img1", "img2"], 
-    steps: ["step1", "step2"], 
+    steps: ["Fold the sheet in half, unfold and then fold the top two corners to the center.",
+     "Fold the top peak down.",
+     "Again, fold the top two edges to the center.",
+     "Fold the top peak to the bottom edge.",
+     "Next, fold the peak back up as shown.",
+     "Fold the plane in half and grab a pair of scissors.",
+     "Now, cut along the grey lines.",
+     "Fold the wings out.",
+     "Finally, fold up the wing tips on the back set of wings."
+    ], 
     items: ["item1", "item2"])
 
-
-
-r1 = Reference.create(
-post_id: 1,   
-href: ("gfhcgvjhbkjln"))
-
-r2 = Reference.create(
-    post_id: 2,
-    href: ("trdyufyiuoihughv"))
